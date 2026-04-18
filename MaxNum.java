838afcffded9a00269fcce7af3f85218b4635109
Rohit Singh Bisht <rohitbisht1603@gmail.com>
2026-04-18 21:18:06 +0530

    Add files via upload

---
+++ MaxNum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MaxNum{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number=");
+        int a = sc.nextInt();
+        System.out.println("enter second number=");
+        int b = sc.nextInt();
+        System.out.println("enter third number=");
+        int c = sc.nextInt();
+        if(a>b&&a>c){
+            System.out.println("a is greater");
+        }else if(b>a&&b>c){
+            System.out.println("b is greater");
+        
+        }
+        else{
+            System.out.println("c is greater");
+        }
+    }
+
+}
